ac1e046b44b75b88053c7137a760aab3e3254ed7
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2018-11-13 09:27:45 +0200

    Viikon 3 tehtävä 1

---
+++ Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -0,0 +1,41 @@
+package ohtu;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.fluent.Request;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // ÄLÄ laita githubiin omaa opiskelijanumeroasi
+        String studentNr = "012345678";
+        if (args.length > 0) {
+            studentNr = args[0];
+        }
+
+        String url = "https://studies.cs.helsinki.fi/courses/students/" + studentNr + "/submissions";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+        
+        System.out.println("json-muotoinen data:");
+        System.out.println(bodyText);
+        System.out.println("");
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+
+        System.out.println("opiskelijanumero " + studentNr + "\n");
+        
+        int totalHours = 0;
+        int exercisesDone = 0;
+        for (Submission submission : subs) {
+            System.out.println(submission);
+            totalHours += submission.getHours();
+            exercisesDone += submission.getExercises().length;
+        }
+        
+        System.out.println("\nyhteensä: " + exercisesDone + " tehtävää "
+                + totalHours + " tuntia");
+    }
+
+}
---
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -0,0 +1,67 @@
+package ohtu;
+
+public class Submission {
+
+    private String course;
+    private int week;
+    private int hours;
+    private int[] exercises;
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int[] getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(int[] exercises) {
+        this.exercises = exercises;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(course + ",");
+        builder.append(" viikko " + week);
+        builder.append(" tehtyjä tehtäviä yhteensä " + exercises.length);
+        builder.append(" aikaa kului " + hours);
+        builder.append(" tehdyt tehtävät: " + tehdytTehtavat());
+        return builder.toString();
+    }
+
+    private String tehdytTehtavat() {
+        String mj = "";
+        boolean eka = true;
+        for (Integer tehtava : exercises) {
+            if (!eka) {
+                mj += ", ";
+            }
+            mj += tehtava;
+            eka = false;
+
+        }
+        return mj;
+    }
+
+}
