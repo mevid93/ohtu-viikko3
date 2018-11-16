2e50e8d2d6917e49a14f9af7a18a6ca2d5539c23
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2018-11-16 18:06:36 +0200

    Viikon 3 tehtävä 12

--- WebLogin/src/test/java/ohtu/Stepdefs.java
+++ WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -6,13 +6,13 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import static org.junit.Assert.*;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Stepdefs {
 
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver = new HtmlUnitDriver();
     String baseUrl = "http://localhost:4567";
 
     @Given("^login is selected$")
@@ -48,6 +48,44 @@ public class Stepdefs {
         logInWith(username, password);
     }
 
+    @Given("^command new user is selected$")
+    public void command_new_user_is_selected() throws Throwable {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+    }
+
+    @When("^a valid username \"([^\"]*)\" and password \"([^\"]*)\" and matching password confirmation are entered$")
+    public void a_valid_username_and_password_and_matching_password_confirmation_are_entered(String username, String password) throws Throwable {
+        registerWith(username, password, password);
+    }
+
+    @Then("^a new user is created$")
+    public void a_new_user_is_created() throws Throwable {
+        pageHasContent("continue to application mainpage");
+    }
+
+    @When("^a short username \"([^\"]*)\" and password \"([^\"]*)\" and matching password confirmation are entered$")
+    public void an_invalid_username_and_password_and_matching_password_confirmation_are_entered(String username, String password) throws Throwable {
+        registerWith(username, password, password);
+    }
+
+    @Then("^user is not created and error \"([^\"]*)\" is reported$")
+    public void user_is_not_created_and_error_is_reported(String errorMsg) throws Throwable {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        assertTrue(driver.getPageSource().contains(errorMsg));
+    }
+
+    @When("^a valid username \"([^\"]*)\" and short password \"([^\"]*)\" and matching password confirmation are entered$")
+    public void a_valid_username_and_short_password_and_matching_password_confirmation_are_entered(String username, String password) throws Throwable {
+        registerWith(username, password, password);
+    }
+
+    @When("^a valid username \"([^\"]*)\" and valid password \"([^\"]*)\" and non matching password confirmation \"([^\"]*)\" are entered$")
+    public void a_valid_username_and_valid_password_and_non_matching_password_confirmation_are_entered(String username, String password, String passwordConfirmation) throws Throwable {
+        registerWith(username, password, passwordConfirmation);
+    }
+
     @After
     public void tearDown() {
         driver.quit();
@@ -65,6 +103,18 @@ public class Stepdefs {
         element = driver.findElement(By.name("password"));
         element.sendKeys(password);
         element = driver.findElement(By.name("login"));
+        element.submit();
+    }
+
+    private void registerWith(String username, String password, String passwordConfirmation) {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(passwordConfirmation);
+        element = driver.findElement(By.name("signup"));
         element.submit();
     }
 }
